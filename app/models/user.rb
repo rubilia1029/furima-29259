ef71859529c3b13d42527e60f064c84b1c610e9a
rubilia1029 <69662402+rubilia1029@users.noreply.github.com>
2020-09-07 09:25:42 +0900

    ロボコップ

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,26 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-  
+
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'include both letters and numbers'
-   
 
-    with_options presence: true do
+  with_options presence: true do
     validates :nickname
     validates :password_confirmation
     validates :birthdate
-    end
+  end
 
-    with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' } do
     validates :family_name
     validates :first_name
-    end
+  end
 
-    with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width katakana characters' } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width katakana characters' } do
     validates :family_name_kana
     validates :first_name_kana
-    end
-  
+  end
 end
