304a1dcf6a50cd33881984c528ea2c6d23eed3ca
rubilia1029 <69662402+rubilia1029@users.noreply.github.com>
2020-09-10 14:52:54 +0900

    修正１

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,9 +7,14 @@ class Item < ApplicationRecord
   belongs_to_active_hash :day
   has_one_attached :image
 
-  validates :image, :name, :explain, :price, presence: true
-  validates :category_id, :condition_id, :fee_id, :day_id, numericality:{ other_than: 1, message: "Select"}
-  validates :area_id, numericality:{ other_than: 0, message: "Select"}
-  validates :price, format: {with: /\A[0-9]+\z/, message: "Half-width number."}
-  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "Out of setting range"}
+  with_options presence: true do
+    validates :image, :name, :explain
+    validates :category_id, numericality: { other_than: 1, message: 'Select' }
+    validates :condition_id, numericality: { other_than: 1, message: 'Select'}
+    validates :fee_id, numericality: { other_than: 1, message: 'Select'}
+    validates :day_id, numericality: { other_than: 1, message: 'Select'}
+    validates :area_id, numericality: { other_than: 0, message: 'Select'}
+    validates :price, numericality: { with: /\A[0-9]+\z/, message: 'Half-width number.'}
+  end
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range'}
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -5,73 +5,84 @@ describe Item, type: :model do
     @item = FactoryBot.build(:item)
   end
 
-  it 'すべての値が正しく入力されていれば保存できる' do
-    expect(@item).to be_valid
-  end
+  describe '商品出品' do
+    context '出品成功' do
+      it 'すべての値が正しく入力されていれば保存できる' do
+        expect(@item).to be_valid
+      end
 
-  it 'imageが空だと保存できない' do
-    @item.image = nil
-    @item.valid?
-    expect(@item.errors.full_messages).to include("Image can't be blank")
-  end
+      it 'priceが半角数字で登録できる' do
+        @item.price = '2000'
+        expect(@item).to be_valid
+      end
+    end
 
-  it 'nameが空だと保存できない' do
-    @item.name = nil
-    @item.valid?
-    expect(@item.errors.full_messages).to include("Name can't be blank")
-  end
+    context '出品失敗' do
+      it 'imageが空だと保存できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
 
-  it 'explainが空だと保存できない' do
-    @item.explain = nil
-    @item.valid?
-    expect(@item.errors.full_messages).to include("Explain can't be blank")
-  end
+      it 'nameが空だと保存できない' do
+        @item.name = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
 
-  it 'Priceが空だと保存できない' do
-    @item.price = nil
-    @item.valid?
-    expect(@item.errors.full_messages).to include("Price can't be blank")
-  end
+      it 'explainが空だと保存できない' do
+        @item.explain = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Explain can't be blank")
+      end
 
-  it 'Priceが半角数字ではないと保存できない' do
-    @item.price = '２０００'
-    @item.valid?
-    expect(@item.errors.full_messages).to include('Price Half-width number.')
-  end
+      it 'Priceが空だと保存できない' do
+        @item.price = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
 
-  it 'Priceが300以上9999999以下でないと保存できない' do
-    @item.price = 200
-    @item.valid?
-    expect(@item.errors.full_messages).to include('Price Out of setting range')
-  end
+      it 'Priceが半角数字ではないと保存できない' do
+        @item.price = '２０００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price Half-width number.')
+      end
 
-  it 'Categoryが選択されてないと保存できない' do
-    @item.category_id = 1
-    @item.valid?
-    expect(@item.errors.full_messages).to include('Category Select')
-  end
+      it 'Priceが300以上9999999以下でないと保存できない' do
+        @item.price = 200
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price Out of setting range')
+      end
 
-  it 'Conditionが選択されてないと保存できない' do
-    @item.condition_id = 1
-    @item.valid?
-    expect(@item.errors.full_messages).to include('Condition Select')
-  end
+      it 'Categoryが選択されてないと保存できない' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Category Select')
+      end
 
-  it 'Feeが選択されてないと保存できない' do
-    @item.fee_id = 1
-    @item.valid?
-    expect(@item.errors.full_messages).to include('Fee Select')
-  end
+      it 'Conditionが選択されてないと保存できない' do
+        @item.condition_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Condition Select')
+      end
 
-  it 'Day が選択されてないと保存できない' do
-    @item.day_id = 1
-    @item.valid?
-    expect(@item.errors.full_messages).to include('Day Select')
-  end
+      it 'Feeが選択されてないと保存できない' do
+        @item.fee_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Fee Select')
+      end
 
-  it 'Areaが選択されてないと保存できない' do
-    @item.area_id = 0
-    @item.valid?
-    expect(@item.errors.full_messages).to include('Area Select')
+      it 'Day が選択されてないと保存できない' do
+        @item.day_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Day Select')
+      end
+
+      it 'Areaが選択されてないと保存できない' do
+        @item.area_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Area Select')
+      end
+    end
   end
 end
